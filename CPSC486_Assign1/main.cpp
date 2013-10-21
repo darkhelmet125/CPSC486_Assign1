77d077c239469e30cbe69e65d824e1eded43ad86
Matthew Short <darkhelmet125@yahoo.com>
2013-10-21 14:16:59 -0700

    Initial Commit

---
+++ CPSC486_Assign1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CPSC486_Assign1
+//
+//  Created by William Short on 10/21/13.
+//  Copyright (c) 2013 William Short. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
